5003e5b130314a47fe0a327fb21caddbc5954f62
Nick <walkmanaly@gmail.com>
2018-07-31 22:49:50 +0800

    load方法
    在执行子类的load方法之前，必定会先执行所有超类的load方法

---
+++ 11111/TestLoad/HGMStudent.m
@@ -0,0 +1,17 @@
+//
+//  HGMStudent.m
+//  11111
+//
+//  Created by Nick on 2018/7/31.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import "HGMStudent.h"
+
+@implementation HGMStudent
+
++ (void)load {
+    NSLog(@"student load");
+}
+
+@end
