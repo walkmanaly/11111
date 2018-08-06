7ffb0017ffc1f93d68c00b718d6ea7bdc8433d2e
Nick <walkmanaly@gmail.com>
2018-08-07 00:05:25 +0800

    方法列表
    runtime获取方法列表

--- 11111/Controllers/ViewController.m
+++ 11111/Controllers/ViewController.m
@@ -100,6 +100,22 @@
     // 我们在定义私有方法时，避免使用_开头(苹果私有方法默认以_开头)，防止覆写了父类的私有方法，导致该调的父类方法没有执行，产生错误
     BOOL isResponds = [self respondsToSelector:@selector(_resetViewController)];
     NSLog(@"isResponds---%d", isResponds);
+    
+    Class clz = [self class];
+    unsigned int methodCount = 0;
+    Method *methods = class_copyMethodList(clz, &methodCount);
+    printf("Found %d methods on '%s'\n", methodCount, class_getName(clz));
+    for (unsigned int i = 0; i < methodCount; i++) {
+        Method method = methods[i];
+        printf("\t'%s' has method named '%s' of encoding '%s'\n",
+               class_getName(clz),
+               sel_getName(method_getName(method)),
+               method_getTypeEncoding(method));
+        /**
+         *  Or do whatever you need here...
+         */
+    }
+    free(methods);
 }
 
 - (void)testDeallocBlock {
