41d51e51190675a564fe4934e343fc4d170b323a
Nick <walkmanaly@gmail.com>
2018-08-21 00:09:07 +0800

    Zombie Object
    僵尸对象_NSZombie_HGMClass
    -fno-objc-arc 将文件改为MRC编译
    -fobjc-arc       将文件改为ARC编译

--- MyLearnCode/main.m
+++ MyLearnCode/main.m
@@ -7,10 +7,37 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 #import "AppDelegate.h"
+#import "HGMClass.h"
+
+void PrintClassInfo(id obj) {
+    Class cls = object_getClass(obj);
+    Class superCls = class_getSuperclass(cls);
+    NSLog(@"---%s : %s---", class_getName(cls), class_getName(superCls));
+}
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        HGMClass *obj = [[HGMClass alloc] init];
+        NSLog(@"before");
+        PrintClassInfo(obj);
+        [obj release];
+        NSLog(@"after");
+        PrintClassInfo(obj);
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
