526d5adbfd539c755db2fdccda03bb0669e98176
Nick <walkmanaly@gmail.com>
2018-08-20 23:51:00 +0800

    ZombieObject
    test ZombieObject

---
+++ MyLearnCode/ZombieObject/HGMClass.h
@@ -0,0 +1,17 @@
+//
+//  HGMClass.h
+//  MyLearnCode
+//
+//  Created by Nick on 2018/8/20.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HGMClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyLearnCode/ZombieObject/HGMClass.m
@@ -0,0 +1,13 @@
+//
+//  HGMClass.m
+//  MyLearnCode
+//
+//  Created by Nick on 2018/8/20.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+#import "HGMClass.h"
+
+@implementation HGMClass
+
+@end
